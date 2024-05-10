05e4c9cf062096e6dbf217e0f14d94a9c7b0f2be
harshjmhr <harshjmhr@gmail.com>
2024-05-10 12:49:10 +0530

    update expert register flow

---
+++ modules/user/constant.js
@@ -0,0 +1,7 @@
+module.exports = {
+    TYPE: {
+        USER: 'user',
+        EXPERT: 'expert'
+    },
+
+};
--- modules/user/service.js
+++ modules/user/service.js
@@ -1,18 +1,32 @@
 const userDetailsModel = require("./model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const { isUndefinedOrNull } = require("../../utils/validators");
+const { TYPE } = require('./constant');
 require("dotenv").config();
 
 const userService = {
   registerUserUtil: async (data) => {
-    const { name, gender, contactNumber, email, password, type} = data;
+    const { name, gender, contactNumber, email, password, type, age, gst, bankName, ifscCode, bankAccountNumber, location} = data;
+    if (type == TYPE.EXPERT && isUndefinedOrNull(gst) && isUndefinedOrNull(bankName) && isUndefinedOrNull(ifscCode) && isUndefinedOrNull(bankAccountNumber)) {
+        throw new Error('details missing from [ gst, bank name, ifsc code, account number ]');
+    } 
     let user = new userDetailsModel();
     user.name = name;
-    user.gender = gender;
+    if (!isUndefinedOrNull(gender)) user.gender = gender;
+    if (!isUndefinedOrNull(age)) user.age = age;
     user.contactNumber = contactNumber;
     user.email = email;
     user.password = password;
     user.type = type;
+    user.location = location;
+
+    if (type == TYPE.EXPERT) {
+        user.gst = gst;
+        user.bankName = bankName;
+        user.ifscCode = ifscCode;
+        user.bankAccountNumber = bankAccountNumber;
+    }
 
     user.password = await bcrypt.hash(user.password, 10);
     savedUser = await user.save();
