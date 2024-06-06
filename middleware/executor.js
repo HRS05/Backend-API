58c218a4e7bc3b117b3798c0044cd27c5d2f155f
harshjmhr <harshjmhr@gmail.com>
2024-06-06 22:10:34 +0530

    fix log

--- middleware/executor.js
+++ middleware/executor.js
@@ -5,18 +5,20 @@ const execute = (func) => {
   return async (req, res, next) => {
     const startTime = Date.now();
     const apiPath = req.path;
+    let message = "";
     try {
       const result = await func(req, res);
       const endTime = Date.now();
       const duration = endTime - startTime;
-      Logger.info(`API ${apiPath} called and took ${duration}ms`);
+      message += `| API -> ${apiPath} | TIME -> ${duration}ms `;
+      if (req?.user?.user_id) message += `| USER -> ${req?.user?.user_id} |`;
+      Logger.info(message);
       return res.status(200).send({ status: 200, data: result });
     } catch (e) {
       const endTime = Date.now();
       const duration = endTime - startTime;
-      Logger.error(
-        `API ${apiPath} called and failed after ${duration}ms: ${e.message}`
-      );
+      message += `| API -> ${apiPath} | TIME -> ${duration}ms | ERROR -> ${e.message} |`;
+      Logger.error(message);
       return res.status(400).send({ status: 400, error: e.message });
     }
   };
