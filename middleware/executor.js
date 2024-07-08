62efd5e3a8f9298a038bcce560d3952d05af18e6
harshjmhr <harshjmhr@gmail.com>
2024-07-09 01:59:28 +0530

    fix issue

--- middleware/executor.js
+++ middleware/executor.js
@@ -12,8 +12,8 @@ const execute = (func) => {
       const endTime = Date.now();
       const duration = endTime - startTime;
       message += `| API -> ${apiPath} | TIME -> ${duration}ms `;
-      if (!isUndefinedOrNull(req.query)) message += `| query: ${JSON.parse(req.query)} `;
-      if (!isUndefinedOrNull(req.body)) message += `| body: ${JSON.parse(req.body)} `;
+      if (!isUndefinedOrNull(req.query)) message += `| query: ${req.query} `;
+      if (!isUndefinedOrNull(req.body)) message += `| body: ${req.body} `;
       if (req?.user?.user_id) message += `| USER -> ${req?.user?.user_id} |`;
       Logger.info(message);
       return res.status(200).send({ status: 200, data: result });
@@ -21,8 +21,8 @@ const execute = (func) => {
       const endTime = Date.now();
       const duration = endTime - startTime;
       message += `| API -> ${apiPath} | TIME -> ${duration}ms | ERROR -> ${e.message} |`;
-      if (!isUndefinedOrNull(req.query)) message += `| query: ${JSON.parse(req.query)} `;
-      if (!isUndefinedOrNull(req.body)) message += `| body: ${JSON.parse(req.body)} `;
+      if (!isUndefinedOrNull(req.query)) message += `| query: ${req.query} `;
+      if (!isUndefinedOrNull(req.body)) message += `| body: ${req.body} `;
       Logger.error(message);
       return res.status(400).send({ status: 400, error: e.message });
     }
